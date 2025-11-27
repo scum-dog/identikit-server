f73f419cf4044524197a6df551201e4f9ae07371
John Q. Herman <johnqherman@gmail.com>
2025-11-26 21:31:08 -0500

    add `data:` scheme to img-src CSP

--- src/routes/auth/itch.ts
+++ src/routes/auth/itch.ts
@@ -324,7 +324,7 @@ router.get("/callback", (req: Request, res: Response) => {
   res.setHeader("Referrer-Policy", "strict-origin-when-cross-origin");
   res.setHeader(
     "Content-Security-Policy",
-    "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://unpkg.com; img-src 'self'; form-action 'none'; frame-ancestors 'none';",
+    "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://unpkg.com; img-src 'self' data:; form-action 'none'; frame-ancestors 'none';",
   );
   res.send(html);
 });
